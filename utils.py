77c75268cc279d7e34de205e4ff47f1d35fc2db6
NischayYadav615 <112478816+NischayYadav615@users.noreply.github.com>
2023-03-21 16:04:43 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -382,36 +382,21 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-
     url = f'https://Clicksfly.com/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
-              'format': 'json'
               }
-              
-try:
 
+    try:
         async with aiohttp.ClientSession() as session:
-
             async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-
-                data = await response.json(content_type='text/html')
-
+                data = await response.json()
                 if data["status"] == "success":
-
-                    return data['shortlink']
-
+                    return data['shortenedUrl']
                 else:
-
                     logger.error(f"Error: {data['message']}")
-
-                    return f'https://Clicksfly.com/api?api={URL_SHORTNER_WEBSITE_API}&link={link}'
+                    return f'https://{URL_SHORTENR_WEBSITE}/api?api={URL_SHORTNER_WEBSITE_API}&link={link}'
 
     except Exception as e:
-
         logger.error(e)
-
-        return f'https://Clicksfly.com/api?api={URL_SHORTNER_WEBSITE_API}&link={link}'
-
-
-    
+        return f'{URL_SHORTENR_WEBSITE}/api?api={URL_SHORTNER_WEBSITE_API}&link={link}'
